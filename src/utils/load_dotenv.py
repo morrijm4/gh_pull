db4ed4828d3713916eb28940c7409c8f1b557798
Matthew Morrison <matthew@mattymo.dev>
2026-03-24 11:22:39 -0400

    find .env at root of gh_pull

--- src/utils/load_dotenv.py
+++ src/utils/load_dotenv.py
@@ -1,9 +1,11 @@
 import os
+from pathlib import Path
 
 
 def load_dotenv():
     try:
-        with open(".env", "r") as file:
+        current_file = Path(__file__).resolve()
+        with open(current_file.parent.parent / ".env", "r") as file:
             for line in file:
                 key, *value = line.split("=")
                 os.environ[key] = "".join(value).removesuffix("\n")
