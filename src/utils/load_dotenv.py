d5571225ed6745b0faf3a5f48b52a54f141aee1c
Matthew Morrison <matthew@mattymo.dev>
2026-04-18 12:34:05 -0400

    fix .env path

--- src/utils/load_dotenv.py
+++ src/utils/load_dotenv.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def load_dotenv():
     try:
         current_file = Path(__file__).resolve()
-        with open(current_file.parent.parent / ".env", "r") as file:
+        with open(current_file.parent.parent.parent / ".env", "r") as file:
             for line in file:
                 key, *value = line.split("=")
                 os.environ[key] = "".join(value).removesuffix("\n")
